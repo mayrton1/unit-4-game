80f76b8256de317d5e85c51a3033d41d4e19831d
mayrton <ayrtonmatthew@yahoo.com>
2019-09-07 15:15:40 -0600

    added updates to CSS, java scritp, jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,11 +13,25 @@ console.log("Hello World");
 var random_result;
 var lost;
 var win;
+random_result = Math.floor(Math.random() * 38) + 1; //hosting
+console.log(random_result);
+
+$("#result").html("Random Result:"+ random_result);
 
 for (var i = 0; i < 4; i++) {
+    var random = Math.floor(Math.random() * 11) +1;
+    // console.log(random);
+
     var crystal = $("<div>");
-        crystal.attr("class",'crystal');
+    crystal.attr({
+        "class": 'crystal', "data-random": random
+    });
     $(".cyrstals").append(crystal);
 
-    
+
 }
+
+$(".cyrstals").on("click",function(){
+
+    console.log($(this).attr("data-random"));
+});
