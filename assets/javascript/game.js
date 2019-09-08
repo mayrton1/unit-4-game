5c73fa63cd64f8109c71a261533d2bae47ca8b2b
mayrton <ayrtonmatthew@yahoo.com>
2019-09-07 22:11:06 -0600

    added game.js jquery code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-console.log("Hello World");
+// console.log("Hello World");
 
 // Pseudocode
 // Game with 4 cyrstals and random result
@@ -13,17 +13,18 @@ console.log("Hello World");
 var random_result;
 var lost;
 var win;
+var previous = 0;
 random_result = Math.floor(Math.random() * 38) + 1; //hosting
 console.log(random_result);
 
 $("#result").html("Random Result:"+ random_result);
 
 for (var i = 0; i < 4; i++) {
-    var random = Math.floor(Math.random() * 11) +1;
-    // console.log(random);
+    var random = Math.floor(Math.random() * 12) +1;
+     console.log(random);
 
     var crystal = $("<div>");
-    crystal.attr({
+        crystal.attr({
         "class": 'crystal', "data-random": random
     });
     $(".cyrstals").append(crystal);
@@ -31,7 +32,17 @@ for (var i = 0; i < 4; i++) {
 
 }
 
-$(".cyrstals").on("click",function(){
+$(".cyrstal").on('click',function(){
 
-    console.log($(this).attr("data-random"));
+    var num = parseInt($(this).attr('data-random'));
+    
+    previous+= num;
+
+    if(previous > random_result){
+        console.log("you lose");
+    }
+    else if( previous ===random_result){
+        console.log("you win");
+    }
+    console.log(previous);
 });
