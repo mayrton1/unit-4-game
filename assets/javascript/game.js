64e29cf20ba3471b202dc7268561d7c694e93804
mayrton <ayrtonmatthew@yahoo.com>
2019-09-08 13:07:34 -0600

    added more javas script

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,37 +11,51 @@
 // If it is equal then the win counter increases
 
 var random_result;
-var lost;
-var win;
+var lost = 0;
+var win = 0;
 var previous = 0;
-random_result = Math.floor(Math.random() * 38) + 1; //hosting
+random_result = Math.floor(Math.random() * 101) + 19; //hosting
 console.log(random_result);
 
-$("#result").html("Random Result:"+ random_result);
+$("#result").html("Random Result:" + random_result);
+
+// Game with 4 cyrstals and random result
+// Every crystal needs to have a random number between 1-12
 
 for (var i = 0; i < 4; i++) {
-    var random = Math.floor(Math.random() * 12) +1;
-     console.log(random);
+    var random = Math.floor(Math.random() * 11) + 1;
+    console.log(random);
 
     var crystal = $("<div>");
-        crystal.attr({
+    crystal.attr({
         "class": 'crystal', "data-random": random
     });
+
+    crystal.html(random);
+
     $(".cyrstals").append(crystal);
 
 
 }
 
-$(".cyrstal").on('click',function(){
+$(".cyrstal").on('click', function () {
 
     var num = parseInt($(this).attr('data-random'));
-    
-    previous+= num;
 
-    if(previous > random_result){
+    previous += num;
+
+    // A new random number should be generated every single time after a win or loss
+    // to those 4 cyrstals 
+    // If it is greater than the random result it displays a lost counter 
+    if (previous > random_result) {
+        lost--;
+        $("#loss").html(loss);
         console.log("you lose");
     }
-    else if( previous ===random_result){
+    // If it is equal then the win counter increases
+    else if (previous === random_result) {
+        win++;
+        $("#win").html(win);
         console.log("you win");
     }
     console.log(previous);
