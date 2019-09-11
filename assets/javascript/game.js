bb45bbeb2c1a15dae6b3ef89d27b2794b0ad3bdd
mayrton <ayrtonmatthew@yahoo.com>
2019-09-11 00:23:27 -0600

    added css, java scritp and html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,56 +14,86 @@ var random_result;
 var lost = 0;
 var win = 0;
 var previous = 0;
-random_result = Math.floor(Math.random() * 101) + 19; //hosting
-console.log(random_result);
 
-$("#result").html("Random Result:" + random_result);
-$('#result').css({"color" : "lightgreen"});
+var restAndStart = function () {
 
-// Game with 4 cyrstals and random result
-// Every crystal needs to have a random number between 1-12
+    $(".crystals").empty();
 
-for (var i = 0; i < 4; i++) {
-    var random = Math.floor(Math.random() * 11) + 1;
-    console.log(random);
+    var images = ["C:/Users/ayrto/OneDrive/Desktop/unit-4-game/assets/images/Gemston1.jpg",
+     "C:/Users/ayrto/OneDrive/Desktop/unit-4-game/assets/images/Gemstone 2.jpg", 
+     "",
+     ""
+    ];
 
-    var crystal = $("<div>");
-    crystal.attr({
-        "class": 'crystal', "data-random": random
-    });
+    random_result = Math.floor(Math.random() * 101) + 19; //hosting
+    //console.log(random_result);
 
-    crystal.html(random);
+    $("#result").html("Random Result:" + random_result);
+    $('#result').css({ "color": "lightgreen" });
 
-    $(".crystals").append(crystal);
+    // Game with 4 cyrstals and random result
+    // Every crystal needs to have a random number between 1-12
 
+    for (var i = 0; i < 4; i++) {
 
+        var random = Math.floor(Math.random() * 11) + 1;
+        console.log(random);
+
+        var crystal = $("<div>");
+        crystal.attr({
+            "class": 'crystal', "data-random": random
+        });
+        crystal.css({
+            "background-image":"url('" + (images[i]) + "')",
+            
+        });
+        // crystal.html(random);  //crystal numbers in div. 
+
+        $(".crystals").append(crystal);
+    }
+    $("#previous").html("Total Score:" + previous);
 }
 
-$(".crystal").on('click', function () {
+restAndStart();
 
-    console.log("crystalclick");
+
+
+$(document).on('click', ".crystal", function () {
+
+    //  console.log("crystalclick");
 
     var num = parseInt($(this).attr('data-random'));
 
     previous += num;
 
+    $("#previous").html("Total Score:" + previous);
+
     // A new random number should be generated every single time after a win or loss
     // to those 4 cyrstals 
     // If it is greater than the random result it displays a lost counter 
     if (previous > random_result) {
-        lost--;
-        $("#loss").html(loss);
-        console.log("you lose");
+        lost++;
+        $("#lost").html("You lost:" + lost);
+
+        previous = 0;
+
+
+        // console.log("you lose");
+        restAndStart();
     }
     // If it is equal then the win counter increases
     else if (previous === random_result) {
         win++;
-        $("#win").html(win);
-        console.log("you win");
+        $("#win").html("You win!:" + win);
+
+        previous = 0;
+
+        restAndStart();
+        // console.log("you win");
     }
-    console.log(previous);
+    // console.log(previous);
 });
 
-function newFunction() {
-    return "random";
-}
+// function newFunction() {
+//     return "random";
+// }
